e1e96559c0b5f0668b6be2ec3ea5a9d2481d8016
xiaoguzai <31726901+boss2020@users.noreply.github.com>
2020-12-11 11:29:35 +0800

    Update dial.py

--- dial/dial.py
+++ dial/dial.py
@@ -4,8 +4,8 @@ url = 'http://10.3.8.211/login'
 dial_url = 'http://10.3.8.211/dial'
 
 data = {
-  'user': '2018110737',
-  'pass': 'kuaile9696',
+  'user': '',
+  'pass': '',
   'line': 'CUC-BRAS'
 }
 
@@ -16,3 +16,5 @@ if __name__ == "__main__":
         r_dial = s.get(dial_url)
         if r_dial.json()['code']== 0:
             break
+#进入北邮登录界面模拟登录，登录成功之后对应的
+#'code'的相应属性为0
